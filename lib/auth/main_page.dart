f6417cefd23d5d3fc5f5cd4c50014c1cbc093f96
Ahmet Buğra <its4thaxeon@outlook.com>
2024-10-28 02:12:01 +0300

    created register page and made user configurations

---
+++ lib/auth/main_page.dart
@@ -0,0 +1,24 @@
+import 'package:collectionapp/auth/auth_page.dart';
+import 'package:collectionapp/pages/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage(); // the page that appears if the user already logged in
+          } else {
+            return AuthPage(); // the page that appears if the user is not yet logged in
+          }
+        },
+      ),
+    );
+  }
+}
