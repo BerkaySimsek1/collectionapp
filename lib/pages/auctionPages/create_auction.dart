36317ed963504c0ba63746d6114bb8cff23a7f53
Ahmet Buğra <its4thaxeon@outlook.com>
2024-11-05 01:16:09 +0300

    made dialog adjustments

--- lib/pages/auctionPages/create_auction.dart
+++ lib/pages/auctionPages/create_auction.dart
@@ -76,7 +76,14 @@ class _AuctionUploadScreenState extends State<AuctionUploadScreen> {
         print("Yükleme sırasında hata oluştu: $e");
       }
     } else {
-      print("Form geçerli değil, resim veya süre seçilmedi.");
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            content: Text("Form geçerli değil, eksik bilgileri tamamlayınız."),
+          );
+        },
+      );
     }
   }
 
@@ -85,60 +92,76 @@ class _AuctionUploadScreenState extends State<AuctionUploadScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text("Auction Upload")),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(20),
         child: Form(
           key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _nameController,
-                decoration: const InputDecoration(labelText: "Auction Name"),
-                validator: (value) =>
-                    value!.isEmpty ? "This field is required" : null,
-              ),
-              TextFormField(
-                controller: _priceController,
-                decoration: const InputDecoration(labelText: "Starting Price"),
-                keyboardType: TextInputType.number,
-                validator: (value) =>
-                    value!.isEmpty ? "This field is required" : null,
-              ),
-              TextFormField(
-                controller: _descriptionController,
-                decoration: const InputDecoration(labelText: "Description"),
-                maxLines: 3,
-                validator: (value) =>
-                    value!.isEmpty ? "This field is required" : null,
-              ),
-              const SizedBox(height: 10),
-              DropdownButton<int>(
-                value: _selectedDays,
-                items: const [
-                  DropdownMenuItem(value: 1, child: Text("1 Day")),
-                  DropdownMenuItem(value: 2, child: Text("2 Days")),
-                  DropdownMenuItem(value: 3, child: Text("3 Days")),
-                ],
-                onChanged: (value) {
-                  setState(() {
-                    _selectedDays = value!;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: _pickImages,
-                child: const Text("Select Images"),
-              ),
-              Wrap(
-                children: _selectedImages.map((image) {
-                  return Image.file(File(image.path), width: 100, height: 100);
-                }).toList(),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _uploadAuction,
-                child: const Text("Upload Auction"),
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: "Auction Name"),
+                  validator: (value) =>
+                      value!.isEmpty ? "This field is required" : null,
+                ),
+                TextFormField(
+                  controller: _priceController,
+                  decoration:
+                      const InputDecoration(labelText: "Starting Price"),
+                  keyboardType: TextInputType.number,
+                  validator: (value) =>
+                      value!.isEmpty ? "This field is required" : null,
+                ),
+                TextFormField(
+                  controller: _descriptionController,
+                  decoration: const InputDecoration(labelText: "Description"),
+                  maxLines: 3,
+                  validator: (value) =>
+                      value!.isEmpty ? "This field is required" : null,
+                ),
+                const SizedBox(height: 10),
+                DropdownButton<int>(
+                  value: _selectedDays,
+                  items: const [
+                    DropdownMenuItem(value: 1, child: Text("1 Day")),
+                    DropdownMenuItem(value: 2, child: Text("2 Days")),
+                    DropdownMenuItem(value: 3, child: Text("3 Days")),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedDays = value!;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: _pickImages,
+                  child: const Text("Select Images"),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        offset: Offset(1, 1),
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Wrap(
+                    children: _selectedImages.map((image) {
+                      return Image.file(File(image.path),
+                          width: 100, height: 100);
+                    }).toList(),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: _uploadAuction,
+                  child: const Text("Upload Auction"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
