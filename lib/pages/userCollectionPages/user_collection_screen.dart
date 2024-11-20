a3d56ff55d9a336fb4c4508e3594e2d2835a3d93
BerkaySimsek1 <113512628+BerkaySimsek1@users.noreply.github.com>
2024-11-20 14:28:51 +0300

    creating user collection tracker logic

---
+++ lib/pages/userCollectionPages/user_collection_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'add_collection_screen.dart';
+import 'collection_items_screen.dart';
+
+class UserCollectionsScreen extends StatelessWidget {
+  final String userId;
+
+  const UserCollectionsScreen({Key? key, required this.userId})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Koleksiyonlarım')),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('userCollections')
+            .doc(userId)
+            .collection('collections')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(child: Text('Henüz bir koleksiyon eklenmemiş.'));
+          }
+
+          final collections = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: collections.length,
+            itemBuilder: (context, index) {
+              final collection = collections[index];
+              return ListTile(
+                title: Text(collection['name']),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CollectionItemsScreen(
+                        userId: userId,
+                        collectionName: collection['name'],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddCollectionScreen(userId: userId),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
