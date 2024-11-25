f6e9508c098fb5c11e6cf9d9e43b9a76df26cb14
Ahmet Buğra <its4thaxeon@outlook.com>
2024-11-26 02:08:30 +0300

    made UI adjustments

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -34,7 +34,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.grey[200],
       body: Center(
         child: SingleChildScrollView(
           child: Column(
