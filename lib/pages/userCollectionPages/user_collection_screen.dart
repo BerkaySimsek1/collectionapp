36634084db2ef3fde471e0b5d6b8decfb6c2bac2
Ahmet Buğra <its4thaxeon@outlook.com>
2024-11-30 03:51:41 +0300

    changed UI of auctions and collections pages

--- lib/pages/userCollectionPages/user_collection_screen.dart
+++ lib/pages/userCollectionPages/user_collection_screen.dart
@@ -11,48 +11,110 @@ class UserCollectionsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          'My Collections',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.deepPurple,
+          ),
+        ),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.deepPurple,
+            )),
+      ),
       backgroundColor: Colors.grey[200],
-      appBar: AppBar(title: const Text('Koleksiyonlarım')),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('userCollections')
-            .doc(userId)
-            .collection('collections')
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Üstte Karşılama Mesajı
+            const SizedBox(height: 8),
+            Text(
+              'You can manage all your collections or add new ones here.',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[700],
+              ),
+            ),
+            const SizedBox(height: 16),
+            // Koleksiyonlar Listesi
+            Expanded(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('userCollections')
+                    .doc(userId)
+                    .collection('collections')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
 
-          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return const Center(
-                child: Text('Henüz bir koleksiyon eklenmemiş.'));
-          }
-
-          final collections = snapshot.data!.docs;
-
-          return ListView.builder(
-            itemCount: collections.length,
-            itemBuilder: (context, index) {
-              final collection = collections[index];
-              return ListTile(
-                title: Text(collection['name']),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CollectionItemsScreen(
-                        userId: userId,
-                        collectionName: collection['name'],
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return const Center(
+                      child: Text(
+                        'Henüz bir koleksiyon eklenmemiş.',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
                       ),
-                    ),
+                    );
+                  }
+
+                  final collections = snapshot.data!.docs;
+                  // Kaydırılabilir sekme
+                  return ListView.builder(
+                    itemCount: collections.length,
+                    itemBuilder: (context, index) {
+                      final collection = collections[index];
+                      // Koleksiyon kartları
+                      return Card(
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16)),
+                        child: ListTile(
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 8),
+                          title: Text(
+                            collection['name'],
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          trailing: const Icon(Icons.arrow_forward_ios,
+                              color: Colors.deepPurple),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => CollectionItemsScreen(
+                                  userId: userId,
+                                  collectionName: collection['name'],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    },
                   );
                 },
-              );
-            },
-          );
-        },
+              ),
+            ),
+          ],
+        ),
       ),
+      // Yeni Koleksiyon Ekleme Butonu
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -62,7 +124,8 @@ class UserCollectionsScreen extends StatelessWidget {
             ),
           );
         },
-        child: const Icon(Icons.add),
+        backgroundColor: Colors.deepPurple,
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
