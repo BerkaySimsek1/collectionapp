1dcf19241a25d8efc3fabd81a5f6e2532960aaed
Ahmet Buğra <its4thaxeon@outlook.com>
2025-06-01 04:15:10 +0300

    test commit

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,3 +1,5 @@
+// test commit
+
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
